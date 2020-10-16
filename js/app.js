3ef8b696574cdf978d4c1c7ef135ed9d8d141ed6
RYAN <RTIPPER44@GMAIL.COM>
2020-10-16 16:19:22 -0700

    fixes so hellow world console log was working. yay!

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-console.log('hello world');{
+console.log('hello world'); {
 
 }
 
@@ -22,29 +22,39 @@ var parentElement = document.getElementById('seattle');
 //p.textContent = `${seattle.name} is the location`;
 //article.appendChild(p);
 
-var ul = document.createElement('ul');
-article.appendChild(ul);
-
-for (var i = 0; i < seattle.name.length; i++) {
-  var li = document.createElement('li');
-  li.textContent = seattle.name[i];
-  ul.appendChild(li);
+var seattle = {
+  name: 'Seattle',
+  minCust: 23,
+  maxCust: 65,
+  avgCookieSale: 6.3,
 }
-
-
-
-
 
 var tokyo = {
   name: 'Tokyo',
-  minCust = 3,
-  maxCust = 24,
-  avgCookieSale = 1.2,
+  minCust: 3,
+  maxCust: 24,
+  avgCookieSale: 1.2,
 }
 
 var dubai = {
   name: 'Dubai',
-  minCust = 11,
-  maxCust = 38,
-  avgCookieSale = 3.7,
+  minCust: 11,
+  maxCust: 38,
+  avgCookieSale: 3.7,
 }
+
+var paris = {
+  name: 'Paris',
+  minCust: 20,
+  maxCust: 38,
+  avgCookieSale: 2.3,
+}
+
+var lima = {
+  name: 'Lima',
+  minCust: 2,
+  maxCust: 16,
+  avgCookieSale: 4.6,
+}
+
+// Store Constructor
