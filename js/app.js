5df5093af2b7c6cd30f973769e23059034a7b542
RYAN <RTIPPER44@GMAIL.COM>
2020-10-16 17:15:54 -0700

    running and working fully now

--- js/app.js
+++ js/app.js
@@ -4,70 +4,90 @@ console.log('hello world'); {
 
 }
 
-// var parentElement = document.getElementById('seattle');
-// console.log(parentElement);
-
-//Store Constructor
+///Store Constructor
 function Store(locationName,minCust,maxCust,avgCookieSale){
-  this.minCust = minCust;
-  this.maxCust = maxCust;
-  this.avgCookieSale = avgCookieSale;
-  this.locationName = locationName;
-  this.hoursOfOperation = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
-  this.hourlySales = [];
-  this.totalSales = 0;
-
-  //Generate today's simulated hourly sales
-  this.getHourlySales = function(){
-      if(this.hourlySales === undefined || this.hourlySales.length==0){
-          this.totalSales=0;
-          var customerCount = 0;
-          var salesCount = 0;
-          for(var i=0; i < this.hoursOfOperation.length; i++){
-              var hour = this.hoursOfOperation[i];
-              var numberOfCustomers = this.getRandomCustomerCount();
-              var cookieSales = Math.ceil(numberOfCustomers * this.avgCookieSale);
-
-              //Increment total cookie sales
-              this.totalSales += cookieSales;
-
-              //Add this hours sales to array
-              this.hourlySales.push(hour +": "+cookieSales+" cookies");
-          }
-      }
-      return this.hourlySales;
-  };
-
-  //Generate a random customer count based on the min and max customer counts
-  this.getRandomCustomerCount = function(){
-      return Math.floor(Math.random() * (this.maxCust - this.minCust)) + this.minCust;
-  };
-
-  //Render this store's sales in the DOM
-  this.renderHourlySales = function(){
-      console.log(this.locationName);
-      var sales = this.getHourlySales();
-      for(var i=0; i < sales.length; i++){
-          console.log(sales[i]);
-      }
-      console.log("Total: "+this.totalSales+" cookies");
-  };
+	this.minCust = minCust;
+	this.maxCust = maxCust;
+	this.avgCookieSale = avgCookieSale;
+	this.locationName = locationName;
+	this.hoursOfOperation = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+	this.hourlySales = [];
+	this.totalSales = 0;
+	
+	//Generate today's simulated hourly sales
+	this.getHourlySales = function(){
+		if(this.hourlySales === undefined || this.hourlySales.length==0){
+			this.totalSales=0;
+			var customerCount = 0;
+			var salesCount = 0;
+			for(var i=0; i < this.hoursOfOperation.length; i++){
+				var hour = this.hoursOfOperation[i];
+				var numberOfCustomers = this.getRandomCustomerCount();
+				var cookieSales = Math.ceil(numberOfCustomers * this.avgCookieSale);
+				
+				//Increment total cookie sales
+				this.totalSales += cookieSales;
+				
+				//Add this hours sales to array
+				this.hourlySales.push(hour +": "+cookieSales+" cookies");
+			}
+		}
+		return this.hourlySales;
+	};
+	
+	//Generate a random customer count based on the min and max customer counts
+	this.getRandomCustomerCount = function(){
+		return Math.floor(Math.random() * (this.maxCust - this.minCust)) + this.minCust;
+	};
+	
+	//Render this store's sales in the DOM
+	this.renderHourlySales = function(){
+		
+		//Get parent element
+		var store = document.getElementById(this.locationName.toLowerCase());
+	
+		//Render the locations name
+		var locationHeader = document.createElement('h2');
+		locationHeader.textContent = this.locationName;
+		
+		//Render the houly sales in an unordered list
+		var locationHourlySales = document.createElement('ul');
+		var sales = this.getHourlySales();
+		for(var i=0; i < sales.length; i++){
+			
+			//Render houly sale count
+			var sale = document.createElement('li');
+			sale.textContent = sales[i];
+			
+			//Append sale count to the list of sales
+			locationHourlySales.appendChild(sale);
+		}
+		
+		//Render the total sales
+    var totalSales = document.createElement('li');
+    totalSales.textContent = this.totalSales;
+		locationHourlySales.appendChild(totalSales);
+		
+		//Add everything to the store page element
+		store.appendChild(locationHeader);
+		store.appendChild(locationHourlySales);
+	};
 }
 
-var seattle = new Store("Seattle",23,65,6.3);
-seattle.renderHourlySales();
+  var seattle = new Store("Seattle", 23, 65, 6.3);
+  seattle.renderHourlySales();
 
-var tokyo = new Store("Tokyo",3,24,1.2);
-tokyo.renderHourlySales();
+  var tokyo = new Store("Tokyo", 3, 24, 1.2);
+  tokyo.renderHourlySales();
 
-var dubai = new Store("Dubai",11,38,3.7);
-dubai.renderHourlySales();
+  var dubai = new Store("Dubai", 11, 38, 3.7);
+  dubai.renderHourlySales();
 
-var paris = new Store("Paris",20,38,2.3);
-paris.renderHourlySales();
+  var paris = new Store("Paris", 20, 38, 2.3);
+  paris.renderHourlySales();
 
-var lima = new Store("Lima",2,16,4.6);
-lima.renderHourlySales();
+  var lima = new Store("Lima", 2, 16, 4.6);
+  lima.renderHourlySales();
 
 // the pattern
 // creat element
@@ -122,6 +142,3 @@ lima.renderHourlySales();
 
 
 // Store Constructor
-
-
-
