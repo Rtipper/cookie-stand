00e68302daa36bb7ce551c7dbf5f46e116df7ac4
RYAN <RTIPPER44@GMAIL.COM>
2020-10-19 15:56:24 -0700

    final edits done and working

--- js/app.js
+++ js/app.js
@@ -1,23 +1,51 @@
 'use strict';
 
-// console.log('hello world'); {
+//Instantiate all the stores
+var seattle = new Store("Seattle",23,65,6.3);
+var tokyo = new Store("Tokyo",3,24,1.2);
+var dubai = new Store("Dubai",11,38,3.7);
+var paris = new Store("Paris",20,38,2.3);
+var lima = new Store("Lima",2,16,4.6);
+//Add all the stores to an array to make things easier
+var stores = [seattle, tokyo, dubai, paris, lima];
 
-// }
+//Render the store table
+renderStoreTable(stores);
 
-///Store Constructor
+//Store Constructor
 function Store(locationName,minCust,maxCust,avgCookieSale){
 	this.minCust = minCust;
 	this.maxCust = maxCust;
 	this.avgCookieSale = avgCookieSale;
 	this.locationName = locationName;
 	this.hoursOfOperation = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
-	this.hourlySales = [];
-	this.totalSales = 0;
+	this.hourlySales = new Map();
+	
+	//Returns the number of cookies sold at a specified time
+	this.getSalesAtHour = function(timeOfSale){
+		var cookiesSold = 0;
+		if(this.getHourlySales().has(timeOfSale)){
+			cookiesSold = this.getHourlySales().get(timeOfSale);
+		}
+		return cookiesSold;
+	};
+	
+	//Returns the total number of cookies sold for this store
+	this.getTotalCookiesSold = function(){
+		var totalCookiesSold = 0;
+		var sales = this.getHourlySales().entries();
+		//Loop through all the sales entries from the hourly sales map
+		for(var cookiesSold of sales){
+			totalCookiesSold += cookiesSold;
+		}
+		return totalCookiesSold;
+	};
 	
 	//Generate today's simulated hourly sales
 	this.getHourlySales = function(){
-		if(this.hourlySales === undefined || this.hourlySales.length==0){
-			this.totalSales=0;
+		//If we havent genrated sales yet, create some.
+		//Otherwise, there's no need to generate them again... return what we got.
+		if(this.hourlySales.size < this.hoursOfOperation.length){
 			var customerCount = 0;
 			var salesCount = 0;
 			for(var i=0; i < this.hoursOfOperation.length; i++){
@@ -25,11 +53,9 @@ function Store(locationName,minCust,maxCust,avgCookieSale){
 				var numberOfCustomers = this.getRandomCustomerCount();
 				var cookieSales = Math.ceil(numberOfCustomers * this.avgCookieSale);
 				
-				//Increment total cookie sales
-				this.totalSales += cookieSales;
-				
-				//Add this hours sales to array
-				this.hourlySales.push(hour +": "+cookieSales+" cookies");
+				//Add this hour's sales to the Map 
+				//The hour is the key and the number of cookies sold is the value
+				this.hourlySales.set(hour,cookieSales);
 			}
 		}
 		return this.hourlySales;
@@ -41,149 +67,98 @@ function Store(locationName,minCust,maxCust,avgCookieSale){
 	};
 	
 	//Render this store's sales in the DOM
-	this.renderHourlySales = function(){
+	this.renderHourlySales = function(tableElement){
 		
 		//Get parent element
-		var store = document.getElementById(this.locationName.toLowerCase());
+		var storeTable = tableElement;
+		
+		//Create a new row in the table for this store
+		var storeTableRow = document.createElement('tr')
 	
 		//Render the locations name
-		var locationHeader = document.createElement('h2');
-		locationHeader.textContent = this.locationName;
+		var storeLocation = document.createElement('td');
+		storeLocation.textContent = this.locationName;
 		
-		//Render the houly sales in an unordered list
+		//Append this location name as the first item in this row
+		storeTableRow.appendChild(storeLocation);
+		
+		//Render the houly sales in this store's table row
 		var locationHourlySales = document.createElement('ul');
 		var sales = this.getHourlySales();
-		for(var i=0; i < sales.length; i++){
+		for(var i=0; i < this.hoursOfOperation.length; i++){
+			var hour = this.hoursOfOperation[i];
+			var cookiesSold = this.getSalesAtHour(hour);
 			
-			//Render houly sale count
-			var sale = document.createElement('li');
-			sale.textContent = sales[i];
-			
-			//Append sale count to the list of sales
-			locationHourlySales.appendChild(sale);
+			//Add this hour's sales to the table row
+			var thisHoursSales = document.createElement('td');
+			thisHoursSales.textContent = cookiesSold;
+			storeTableRow.appendChild(thisHoursSales);
 		}
 		
-		//Render the total sales
-    var totalSales = document.createElement('li');
-    totalSales.textContent = "Total: "+this.totalSales+" cookies";
-		locationHourlySales.appendChild(totalSales);
-		
-		//Add everything to the store page element
-		store.appendChild(locationHeader);
-		store.appendChild(locationHourlySales);
+		//Add this store's sales row to the store table
+		storeTable.appendChild(storeTableRow);
 	};
 }
 
-  // Seattle Location
-  var seattle = new Store("Seattle", 23, 65, 6.3);
-  seattle.renderHourlySales();
+//Get the store table. Created method to grab it so I'm not repeating myself
+function getStoreTable(){
+	return document.getElementById("store-table");
+}
 
-  // Tokyo Location
-  var tokyo = new Store("Tokyo", 3, 24, 1.2);
-  tokyo.renderHourlySales();
-
-  // Dubai Location
-  var dubai = new Store("Dubai", 11, 38, 3.7);
-  dubai.renderHourlySales();
-
-  // Paris Location
-  var paris = new Store("Paris", 20, 38, 2.3);
-  paris.renderHourlySales();
-
-  // Lima Location
-  var lima = new Store("Lima", 2, 16, 4.6);
-	lima.renderHourlySales();
+//Render the store table header containing store hours
+function renderStoreTableHeader(){
+	var hoursOfOperation = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 	
-
-	// Working on Render of Table. Might need to include above.
-//	var shopTable = document.getElementById("shopsTable");
-
-//	shopTable.prototype.render = function () {
-		//creat row
-//		var tr = document.createElement('tr');
-//		var td = document.createElement('td');
-//		td.textContent = this.name;
-//		tr.appendChild(td);
-//	}
-
-//	function renderHeader(){
-//		var tr = document.createElement('tr');
-//		var th = document.createElement('th');
-//		th.textContent = 'Name';
-//		tr.appendChild(th);
-
-//		th = document.createElement('th');
-//		th.textContent = 'Interest One';
-//		tr.appendChild(th);
+	//Get parent element
+	var storeTable = getStoreTable();
+	var headerRow = document.createElement('tr');
 	
-//		th = document.createElement('th');
-//		th.textContent = 'Interest Two';
-//		tr.appendChild(th);
+	var blankCell = document.createElement('td');
+	headerRow.appendChild(blankCell);
 	
-//		th = document.createElement('th');
-//		th.textContent = 'Interest Three';
-//		tr.appendChild(th);
+	for(var i=0; i<hoursOfOperation.length; i++){
+		var hourCell = document.createElement('td');
+		hourCell.textContent = hoursOfOperation[i];
+		headerRow.appendChild(hourCell);
+	}
+	storeTable.appendChild(headerRow);
+}
+
+//Render the store table footer containing sales totals
+function renderStoreTableFooter(stores){
+	//Get parent element
+	var hoursOfOperation = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+	var storeTable = getStoreTable();
+	var footerRow = document.createElement('tr');
 	
-//		th = document.createElement('th');
-//		th.textContent = 'Age';
-//		tr.appendChild(th);
+	var totalRowTitle = document.createElement('td');
+	totalRowTitle.textContent = "Totals"
+	footerRow.appendChild(totalRowTitle);
 	
-//		shopTable.appendChild(tr);
-//	}
+	for(var i=0; i<hoursOfOperation.length; i++){
+		var hour = hoursOfOperation[i];
+		var cookiesSoldThisHour = 0;
+		for(var s=0; s<stores.length; s++){
+			var cookiesSoldAtThisStoreAtThisHour = stores[s].getSalesAtHour(hour);
+			cookiesSoldThisHour += cookiesSoldAtThisStoreAtThisHour;
+		}
+		var totalCookiesSoldThisHourAcrossAllStores = document.createElement('td');
+		totalCookiesSoldThisHourAcrossAllStores.textContent = cookiesSoldThisHour;
+		footerRow.appendChild(totalCookiesSoldThisHourAcrossAllStores);
+	}
+	storeTable.appendChild(footerRow);
+}
 
-
-
-
-// the pattern
-// creat element
-// give it content
-// append to the DOM
-// var article = document.createElement('article');
-// parentElement.appendChild(article);
-
-//var h2 = document.createElement('h2');
-//h2.textContent = seattle.name;
-//article.appendChild(h2);
-
-//var p = document.createElement('p');
-//p.textContent = `${seattle.name} is the location`;
-//article.appendChild(p);
-
-// var seattle = {
-//  name: 'Seattle',
-//  minCust: 23,
-//  maxCust: 65,
-//  avgCookieSale: 6.3,
-// }
-
-// var tokyo = {
-//  name: 'Tokyo',
-//  minCust: 3,
-//  maxCust: 24,
-//  avgCookieSale: 1.2,
-// }
-
-//var dubai = {
-//  name: 'Dubai',
-//  minCust: 11,
-//  maxCust: 38,
-//  avgCookieSale: 3.7,
-// }
-
-//var paris = {
-//  name: 'Paris',
-//  minCust: 20,
-//  maxCust: 38,
-//  avgCookieSale: 2.3,
-// }
-
-// var lima = {
-//  name: 'Lima',
-//  minCust: 2,
-//  maxCust: 16,
-//  avgCookieSale: 4.6,
-// }
-
-
-
-// Store Constructor
+//Render an array of stores in a table
+function renderStoreTable(stores){
+	//First: render the table header
+	renderStoreTableHeader();
+	
+	//Second: render each store in a table row inside the store table
+	for(var i=0; i<stores.length; i++){
+		stores[i].renderHourlySales(getStoreTable());
+	}
+	
+	//Third: render the table footer
+	renderStoreTableFooter(stores)
+}
